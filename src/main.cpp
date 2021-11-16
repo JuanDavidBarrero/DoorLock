da6d55df64a67867d3365ca35576e5a558215cdd
Juan David Barrero <JuanDBarrero@hotmail.com>
2021-11-16 12:23:23 -0500

    finger print detection

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,24 @@
 #include <Finger_Sensor.h>
 
 FingerSensor Sensor;
+int personID;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Sensor.initFingerSentor();
+  if (!Sensor.checkDB())
+  {
+    Sensor.enrollFinger(1);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  personID = Sensor.identifingFinger();
+  if(personID != -1){
+    Serial.printf("The finger id correspont to %i \n", personID);
+    delay(3000);
+  }
+  Serial.print("Plese place your finger\n");
 }
