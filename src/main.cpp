f0ded1ca1b204439ca22bb534986342b76d80aca
Juan David Barrero <JuanDBarrero@hotmail.com>
2021-11-17 21:21:56 -0500

    timer added

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,34 @@
 #include <Finger_Sensor.h>
 #include <EasyNextionLibrary.h>
 
+hw_timer_t *timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
 #define NextionScreen Serial1
 EasyNex myNex(NextionScreen);
 
 FingerSensor Sensor;
 int personID = -1;
-unsigned long timePage = 0;
-unsigned long prevtime = 0;
-unsigned long interval = 10000;
+bool timeOut = false;
 String family[127];
+
+void IRAM_ATTR onTimer()
+{
+  portENTER_CRITICAL_ISR(&timerMux);
+  timeOut = true;
+  timerWrite(timer, 0);
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
 
 void setup()
 {
   Serial.begin(115200);
   myNex.begin(9600);
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 10000000, false);
+
   Sensor.initFingerSentor();
   if (!Sensor.checkDB())
   {
@@ -30,21 +44,23 @@ void loop()
 
 void trigger0()
 {
+  timerAlarmWrite(timer, 10000000, false);
+  timerAlarmEnable(timer);
   myNex.writeStr("page 5");
 
   while (personID == -1)
   {
     personID = Sensor.identifingFinger();
-    timePage = millis();
-    if (timePage - prevtime > interval)
+    if (timeOut)
     {
-      prevtime = millis();
       myNex.writeStr("page 9");
       delay(1000);
       myNex.writeStr("page 0");
+      timeOut=false;
       return;
     }
   }
+
   myNex.writeStr("page 1");
   myNex.writeStr("t1.txt", family[personID - 1]);
   personID = -1;
@@ -56,14 +72,15 @@ void trigger1()
 {
   String nombre = myNex.readStr("t1.txt");
   int id = myNex.readNumber("n0.val");
-  if(id == 0) return;
+  if (id == 0 || id > 127)
+    return;
   myNex.writeStr("page 5");
   Sensor.enrollFinger(id);
   myNex.writeStr("page 6");
-  delay(500);
+  delay(1000);
   myNex.writeStr("page 5");
   Sensor.verifyFinger(id);
-  family[id-1] = nombre;
+  family[id - 1] = nombre;
   myNex.writeStr("page 7");
   delay(1500);
   myNex.writeStr("page 2");
